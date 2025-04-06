4fb1fb776434ff3bac35ef0dea0d8d1cb19fce36
rafaaacampelo <rafaccdfranca@gmail.com>
2025-04-06 00:07:57 -0300

    quase lá

--- app.py
+++ app.py
@@ -1,6 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session, url_for
+from flask_mysqldb import MySQL
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
 
 app = Flask(__name__)
+app.secret_key = 'sua_chave_secreta'  # ideal usar do .env
+
+# Configuração do banco de dados MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'seu_usuario'
+app.config['MYSQL_PASSWORD'] = 'sua_senha'
+app.config['MYSQL_DB'] = 'nome_do_seu_banco'
+
+mysql = MySQL(app)
+
+
+@app.route('/cadastro', methods=['GET', 'POST'])
+def cadastro():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        email = request.form['email']
+        senha = generate_password_hash(request.form['senha'])
+
+        cursor = mysql.connection.cursor()
+        cursor.execute(
+            "INSERT INTO usuarios (nome, email, senha) VALUES (%s, %s, %s)", (nome, email, senha))
+        mysql.connection.commit()
+        cursor.close()
+
+        return redirect(url_for('login'))
+
+    return render_template('cadastro.html')
 
 
 @app.route('/')
